3d940f3e3187732206ba367aa8764cb34d66261b
1587CC211pre <preciousconstance7@gmail.com>
2023-10-09 05:51:03 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int b;
+
+for (b = 48; b < 58; b++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
