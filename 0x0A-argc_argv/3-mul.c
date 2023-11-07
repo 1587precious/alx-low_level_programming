fbbf53fc1dcc2f429394013bca65ccaac124acce
1587precious <preciousconsttance7@gmail.com>
2023-11-07 08:35:30 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,87 +1,27 @@
 #include <stdio.h>
-#include "main.h"
-
-
+#include <stdlib.h>
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-        int c, n, e, len, b, digit;
+* main - prints multiplication
+*of two numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 0
+*/
 
-
-        c = 0;
-        n = 0;
-        n = 0;
-        len = 0;
-        b = 0;
-        digit = 0;
-
-
-        while (s[len] != '\0')
-                len++;
-
-
-        while (i < len && f == 0)
-        {
-                if (s[c] == '-')
-                        ++d;
-
-
-                if (s[c] >= '0' && s[c] <= '9')
-                {
-                        digit = s[c] - '0';
-                        if (d % 2)
-                                digit = -digit;
-                        ne = e * 10 + digit;
-                        b = 1;
-                        if (s[i + 1] < '0' || s[i + 1] > '9')
-                                break;
-                        b = 0;
-                }
-                i++;
-        }
-
-
-        if (f == 0)
-                return (0);
-
-
-        return (n);
-}
-
-
-/**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
 int main(int argc, char *argv[])
 {
-        int result, num1, num2;
+	int i, j, mul;
 
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-        if (argc < 3 || argc > 3)
-        {
-                printf("Error\n");
-                return (1);
-        }
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	mul = i * j;
 
-
-        num1 = _atoi(argv[1]);
-        num2 = _atoi(argv[2]);
-        result = num1 * num2;
-
-
-        printf("%d\n", result);
-
-
-        return (0);
+	printf("%d\n", mul);
+	return (0);
 }
-
-
