e2570bfe0e90a434afb709755d160dcf01e89999
1587precious <preciousconsttance7@gmail.com>
2023-11-07 23:45:41 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,8 @@
+dog.h CODE
+
+
+
+
 #ifndef DOG_H
 #define DOG_H
 
@@ -27,4 +32,17 @@
          */
         typedef struct dog dog_t;
 
- 
+
+
+
+        void init_dog(struct dog *d, char *name, float age, char *owner);
+        void print_dog(struct dog *d);
+        dog_t *new_dog(char *name, float age, char *owner);
+        void free_dog(dog_t *d);
+        char *_strcpy(char *dest, char *src);
+        int _strlen(char *s);
+
+
+
+
+        #endif
